df0065cedcc622bfb02d8ffb6d9d6db3eb2d881c
jc449767 <syed.ahmed@my.jcu.edu.au>
2016-12-09 20:25:23 +1000

    commit first time

---
+++ prac03/num denum.py
@@ -0,0 +1,15 @@
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("denominator cannot be 0.try again...")
+        denominator = int(input ("enter the denominator"))
+    fraction = numerator / denominator
+    print (fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+
---
+++ prac03/oddname.py
@@ -0,0 +1,14 @@
+__author__ = 'jc449767'
+"""
+
+"""
+valid = False
+while not valid:
+      name = input("enter your name:")
+      if name!= ""or name.isspace():
+         valid = True
+print(valid)
+
+l = len(name)
+for i in range(0,l,2):
+     print(name[i])
